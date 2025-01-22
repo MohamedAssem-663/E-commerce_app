d2737d22692e2f6708f96ce51d0020d0a2c2f49c
MohamedAssem-663 <mohamedassem663@gmail.com>
2025-01-22 15:14:35 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EcommerceApp());
+}
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
