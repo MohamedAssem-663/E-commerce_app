61e1a3b048602a11a6bc2e0cff9325f15b60b3c2
MohamedAssem-663 <mohamedassem663@gmail.com>
2025-01-22 16:22:28 -0800

    start the app&add colors and theme

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class EcommerceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(),
     );
   }
