94e606681ee05daca8329dc2e57290390cf89bb5
MohamedAssem-663 <mohamedassem663@gmail.com>
2025-01-23 19:15:35 -0800

    first commit

---
+++ lib/core/configs/assets/app_assests.dart
@@ -0,0 +1,9 @@
+class AppAssets {
+  static const logo = 'assets/images/logo.png';
+  static const bell = 'assets/images/bell 1.png';
+  static const cartBag = 'assets/images/cart_bag.png';
+  static const checkOut = 'assets/images/check-out 1.png';
+  static const notfound = 'assets/images/not_found.png';
+  static const orderPlaced = 'assets/images/order_placed.png';
+  static const profile = 'assets/images/profile.png';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/features/splash/presentation/views/widgets/splash_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class EcommerceApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: SplashView(),
     );
   }
 }
